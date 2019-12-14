357d335cba185ca33ad1dc72a3744e0f0421d9d9
Shubham Pandey <shubhampcpandey13@gmail.com>
2019-12-14 22:49:05 +0530

    models commit 1

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 from .managers import UserManager
 
@@ -15,3 +17,41 @@ class User(AbstractBaseUser):
 
     def __str__(self):
         return self.contact
+
+
+class FriendRequest(models.Model):
+    from_user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="user_sent",
+    )
+    to_user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="user_received",
+    )
+
+    message = models.TextField(blank=True)
+    created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.from_user.username} to {self.to_user.username}"
+
+
+class Friend(models.Model):
+    current_user = models.ForeignKey(User, 
+        on_delete=models.CASCADE, 
+        related_name="user")
+    friend = models.ForeignKey(User, 
+        on_delete=models.CASCADE, 
+        related_name="friend")
+    created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.current_user.username} and {self.friend.username}"
+
+    def save(self, *args, **kwargs):
+        # Ensure users can't be friends with themselves
+        if self.current_user == self.friend:
+            raise ValidationError("Users cannot be friends with themselves.")
+        super(Partner, self).save(*args, **kwargs)
